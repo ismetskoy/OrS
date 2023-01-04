92120dea7a22dfe01c59ff4fb0290b26356b7cbb
Ivan <felhat@bk.ru>
2023-01-04 20:40:59 +0300

    update 2.1

--- ORS_v.2.1.py
+++ ORS_v.2.1.py
@@ -1,21 +1,20 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from pathlib import *
-import os , glob , time , logging , pyautogui , win32com.client , py_win_keyboard_layout , win32com.client as win32
+import os , glob , time , logging , py_win_keyboard_layout , win32com.client as win32
 
-logging.basicConfig(filename = "log.log" , level=logging.INFO , format = '%(asctime)s %(levelname)s %(funcName)s || %(message)s')
+logging.basicConfig(filename = "log.log" , level=logging.INFO , format = '%(asctime)s %(levelname)s %(funcName)s || %(message)s') # Логи
 py_win_keyboard_layout.change_foreground_window_keyboard_layout(0x04090409)  # Смена языка на EN
 
 def ORS():  # Работа с ORS
     start_ors = True
     while start_ors:
         try:
-            link = ('http://ors/ors/atm/promise.html')
             driver = webdriver.Edge()
             driver.maximize_window()
+            time.sleep(3)
+            driver.get('http://ors/ors/atm/promise.html')
             time.sleep(5)
-            driver.get(link)
-            time.sleep(10)
             driver.implicitly_wait(240)
             driver.find_element(By.ID, 'searchButton').click()  # Обновить
             driver.implicitly_wait(360)
@@ -34,28 +33,27 @@ def EXL(): # Работа с EXl
         fileors = glob.glob(os.path.join('C:/Users/u_180u6/Downloads/', 'detail_1672*.xlsx')) # Поиск
         for ors in fileors:
             pass
-            xlApp = win32com.client.DispatchEx('Excel.Application')
+            xlApp = win32.Dispatch('Excel.Application')
             wb = xlApp.Workbooks.Open(ors)
-            xlApp.Visible = True
+            xlApp.Visible = False
             xlApp.Run('PERSONAL.XLSB!ORS_v_4_1') # Макрос
-            time.sleep(60)
-            pyautogui.screenshot(r'C:\Users\u_180u6\Downloads\ORS.jpg', region=(0, 0, 1800, 1000))  # Снимок    
+            time.sleep(45)  
             wb.Save() # Сохранение
+            wb.Worksheets("Total").ExportAsFixedFormat(0, 'C:/Users/u_180u6/Downloads/ORS.pdf') # Сохранение в PDF
             xlApp.Quit() # Выход
             logging.info('-----OK-----')
-            time.sleep(10)
+            time.sleep(5)
     except:
             logging.exception(EXL)
             os.system("taskkill /f /im EXCEL.exe")
-            xlApp.Quit()
             
 def Out():  # Отправка в Outlook
     try:
         fileout = glob.glob(os.path.join('C:/Users/u_180u6/Downloads/ORS_*.xlsx')) # Поиск
         for out in fileout:
             pass
-        filejpg = glob.glob(os.path.join('C:/Users/u_180u6/Downloads/ORS*.jpg')) # Поиск
-        for jpg in filejpg:
+        fileors = glob.glob(os.path.join('C:/Users/u_180u6/Downloads', 'ORS.pdf')) # Поиск
+        for pdf in fileors:
             pass
             outlook = win32.Dispatch('outlook.application')
             mail = outlook.CreateItem(0)
@@ -64,7 +62,7 @@ def Out():  # Отправка в Outlook
             mail.Body = 'Расчет ORS на текущую Дату'
             mail.HTMLBody = '<h2>Расчет ORS на текущую Дату</h2>'
             mail.Attachments.Add(out)
-            mail.Attachments.Add(jpg)
+            mail.Attachments.Add(pdf)
             mail.Send() # Отправка почты
             logging.info('-----OK-----')
     except:
@@ -79,19 +77,19 @@ def Out():  # Отправка в Outlook
 
 def Delete():  # Удаление лишнего
     try:
-        time.sleep(300)
+        time.sleep(60)
         fileout = glob.glob(os.path.join('C:/Users/u_180u6/Downloads/ORS_*.xlsx')) # Поиск
         for out in fileout:
             pass
-        filejpg = glob.glob(os.path.join('C:/Users/u_180u6/Downloads/ORS*.jpg')) # Поиск
-        for jpg in filejpg:
+        fileors = glob.glob(os.path.join('C:/Users/u_180u6/Downloads', 'ORS.pdf')) # Поиск
+        for pdf in fileors:
             pass
         fileors = glob.glob(os.path.join('C:/Users/u_180u6/Downloads/', 'detail_1672*.xlsx')) # Поиск
         for ors in fileors:
             pass
         os.remove(ors)
         os.remove(out)
-        os.remove(jpg)
+        os.remove(pdf)
         logging.info('-----OK-----')
     except:
         logging.exception(Delete)
