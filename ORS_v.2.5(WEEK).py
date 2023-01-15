72578d25ae2ed0d8612e0a08639ccae5dc2d7e57
Ivan <felhat@bk.ru>
2023-01-15 20:38:16 +0300

    Update WEEK

---
+++ ORS_v.2.5(WEEK).py
@@ -0,0 +1,128 @@
+from selenium.webdriver.common.by import By
+from datetime import datetime, timedelta
+from selenium import webdriver
+from threading import Thread
+from pathlib import *
+import os , glob , time , logging  , win32com.client as win32
+from selenium.webdriver.common.keys import Keys
+
+logging.basicConfig(filename = "log.log" , level=logging.INFO , format = '%(asctime)s %(levelname)s %(funcName)s || %(message)s') # Логи
+
+
+day = f"{datetime.now()+ timedelta(days=-7):%d-%m-%Y}"
+week = f"{datetime.now()+ timedelta(days=-7):%d-%m-%Y}" " --- " f"{datetime.now() + timedelta(days=-1):%d-%m-%Y} ""\U0001F642""" 
+
+def TimeEXL(): # Kill EXCEL
+    time.sleep(120)
+    os.system("taskkill /f /im EXCEL.exe")
+
+def Poisk(): # Ожидание загрузки файлы detail
+    file_time=time.time()
+    while (time.time() - file_time) < 300: 
+        filenames = glob.glob(os.path.join('C:/Users/u_180u6/Downloads/', 'detail_*.xlsx'))
+        if len(filenames) > 0 :
+            time.sleep(3)
+            break
+    else:
+        logging.exception(ORS)
+        os.system("taskkill /f /im msedgedriver.exe")
+        os.system("taskkill /f /im msedge.exe")
+        time.sleep(15)
+        ORS()
+
+def ORS():  # Работа с сайтом ORSe
+    start_ors = True
+    while start_ors:
+        try:
+            time.sleep(15)
+            driver = webdriver.Edge()
+            driver.maximize_window()
+            time.sleep(3)
+            driver.get('http://ors/ors/atm/promise.html')
+            time.sleep(5)
+            driver.implicitly_wait(240)
+            driver.find_element(By.ID, 'dateFrom').click()
+            time.sleep(5)
+            driver.find_element(By.ID, 'dateFrom').send_keys(day)
+            time.sleep(5)
+            driver.find_element(By.ID, 'dateFrom').send_keys(Keys.RETURN)
+            driver.find_element(By.ID, 'dateFrom').send_keys(Keys.RETURN)
+            start_ors = False
+            time.sleep(5)
+            Poisk()
+            logging.info('-----OK-----')
+        except:
+            logging.exception(ORS)
+            os.system("taskkill /f /im msedgedriver.exe")
+            os.system("taskkill /f /im msedge.exe")
+            time.sleep(15)
+
+def EXL(): # Работа с EXl
+    try:
+        filedet = glob.glob(os.path.join('C:/Users/u_180u6/Downloads/', 'detail_*.xlsx')) # Поиск
+        for det in filedet:
+            pass
+        xlApp = win32.Dispatch('Excel.Application')
+        wb = xlApp.Workbooks.Open(det)
+        xlApp.Visible = False
+        xlApp.Run('PERSONAL.XLSB!ORS_v_4_1') # Макрос
+        time.sleep(60)  
+        wb.Save() # Сохранение
+        wb.Worksheets("Total").ExportAsFixedFormat(0, 'C:/Users/u_180u6/Downloads/ORS.pdf') # Сохранение в PDF
+        xlApp.Quit() # Выход
+        logging.info('-----OK-----')
+        time.sleep(5)
+    except:
+        logging.exception(EXL)
+        os.system("taskkill /f /im EXCEL.exe")
+            
+def Out():  # Отправка в Outlook
+    try:
+        fileors = glob.glob(os.path.join('C:/Users/u_180u6/Downloads/', 'ORS*.xlsx'))
+        for ors in fileors:
+            pass
+        filepdf = glob.glob(os.path.join('C:/Users/u_180u6/Downloads/', 'ORS*.pdf'))
+        for pdf in filepdf:
+            pass
+        filejpg = glob.glob(os.path.join('C:/Users/u_180u6/Downloads/', 'ORS*.jpg'))
+        for jpg in filejpg:
+            pass
+        outlook = win32.Dispatch('outlook.application')
+        mail = outlook.CreateItem(0)
+        mail.To = '' # Отправка почты
+        mail.Subject = 'Расчет ORS' 
+        mail.Body = 'Расчет ORS за {week}'
+        mail.HTMLBody =  "<html><body><h2>Расчет ORS на Дату: {week}<br></h2><img src=""cid:MyId1""></body></html>".format(week=week)
+        attachment = mail.Attachments.Add(jpg)
+        attachment.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001F", "MyId1")
+        mail.Attachments.Add(jpg)
+        mail.Attachments.Add(ors)
+        mail.Attachments.Add(pdf)
+        mail.Send() # Отправка почты
+        logging.info('-----OK-----')
+    except:
+        logging.exception(Out)
+        outlook = win32.Dispatch('outlook.application')
+        mail = outlook.CreateItem(0)
+        mail.To = '' # Отправка почты
+        mail.Subject = 'Неудача подчета ORS'
+        mail.Body = 'Неудача подчета ORS за {week}'
+        mail.HTMLBody = '<html><body><h2>Неудача подчета ORS за: {week}<br></h2><img src=""cid:MyId1""></body></html>'.format(week=week)
+        mail.Send() # Отправка почты  
+
+def Delete():  # Удаление лишнего
+    try:
+        time.sleep(30)
+        filedel = glob.glob(os.path.join
+            ('C:/Users/u_180u6/Downloads/', 'ORS*.xlsx')) + glob.glob(os.path.join
+            ('C:/Users/u_180u6/Downloads/', 'ORS*.jpg')) + glob.glob(os.path.join
+            ('C:/Users/u_180u6/Downloads/', 'ORS*.pdf')) + glob.glob(os.path.join
+            ('C:/Users/u_180u6/Downloads/', 'detail_*.xlsx')) 
+        for delete in filedel:
+            os.remove(delete)
+            pass
+        logging.info('-----OK-----')
+    except:
+        logging.exception(Delete)
+
+start = (ORS(), Thread(target=TimeEXL).start(), EXL(), Out(), Delete()) # Поехали ;)
