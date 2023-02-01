bf26d19cab26c756f43456f825725aa73f764ffd
Ivan <felhat@bk.ru>
2023-02-01 11:25:28 +0300

    Update

--- ORS_v.2.5.py
+++ ORS_v.2.5.py
@@ -5,25 +5,52 @@ from threading import Thread
 from pathlib import *
 import os , glob , time , logging  , win32com.client as win32
 
-logging.basicConfig(filename = "log.log" , level=logging.INFO , format = '%(asctime)s %(levelname)s %(funcName)s || %(message)s') # Логи
+
+if not os.path.exists('C:\\ORS\\log'): # Проверка Пути
+    os.mkdir('C:\\ORS\\log')
+
+logging.basicConfig(filename = "C:\\ORS\\log\\day.log" , level=logging.INFO , format = '%(asctime)s %(levelname)s %(funcName)s || %(message)s') # Логи
+
+logging.info('Path Exists Bro ;)')
 
 day = f"{datetime.now() + timedelta(days=-1):%d.%m.%Y}"" 🚀 "  # Дата
 
+def TimeKill():
+    file_time=time.time()
+    while (time.time() - file_time) < 555: 
+        filenames = glob.glob(os.path.join('C:/Users/*/Downloads/', 'detail_*.xlsx'))
+        if len(filenames) < 1 :
+            logging.info('-----YES Bro-----')
+            time.sleep(10)
+            os.system("taskkill /f /im msedgedriver.exe")
+            os.system("taskkill /f /im msedge.exe")
+            os.system("taskkill /f /im day.exe")
+    else:
+        logging.info('-----NO Bro-----')
+        time.sleep(10)
+        os.system("taskkill /f /im msedgedriver.exe")
+        os.system("taskkill /f /im msedge.exe")
+        os.system("taskkill /f /im day.exe")
+
 def TimeEXL(): # Kill EXCEL
     time.sleep(120)
+    logging.info('-----No Bro-----')
     os.system("taskkill /f /im EXCEL.exe")
 
 def Poisk(): # Ожидание загрузки файлы detail
     file_time=time.time()
     while (time.time() - file_time) < 300: 
-        filenames = glob.glob(os.path.join('C:/Users/*/Downloads/', 'detail_.xlsx'))
+        filenames = glob.glob(os.path.join('C:/Users/*/Downloads/', 'detail_*.xlsx'))
         if len(filenames) > 0 :
             time.sleep(3)
+            logging.info('-----OK-----')
             break
     else:
         logging.exception(ORS)
         os.system("taskkill /f /im msedgedriver.exe")
-        time.sleep(15)
+        os.system("taskkill /f /im msedge.exe")
+        time.sleep(10)
+        Delete()
         ORS()
 
 def ORS():  # Работа с сайтом ORS
@@ -35,28 +62,29 @@ def ORS():  # Работа с сайтом ORS
             time.sleep(3)
             driver.get('http://ors/ors/atm/promise.html')
             time.sleep(5)
-            driver.implicitly_wait(240)
+            driver.implicitly_wait(220)
             driver.find_element(By.ID, 'searchButton').click()  # Обновить
-            time.sleep(5)
-            driver.implicitly_wait(360)
+            time.sleep(3)
+            driver.implicitly_wait(270)
             driver.find_element(By.ID, 'exportButtonDetail').click()  # Детальный отчет
             start_ors = False
             Poisk()
+            driver.quit()
             logging.info('-----OK-----')
         except:
             logging.exception(ORS)
+            os.system("taskkill /f /im msedge.exe")
             os.system("taskkill /f /im msedgedriver.exe")
 
 def EXL(): # Работа с EXl
     try:
-        filedet = glob.glob(os.path.join('C:/Users/*/Downloads/', 'detail_.xlsx')) # Поиск
+        filedet = glob.glob(os.path.join('C:/Users/*/Downloads/', 'detail_*.xlsx')) # Поиск
         for det in filedet:
             pass
         xlApp = win32.Dispatch('Excel.Application')
         wb = xlApp.Workbooks.Open(det)
         xlApp.Visible = False
         xlApp.Run('PERSONAL.XLSB!ORS_v_4_2') # Макрос
-        time.sleep(60)  
         wb.Save() # Сохранение
         xlApp.Quit() # Выход
         logging.info('-----OK-----')
@@ -64,20 +92,20 @@ def EXL(): # Работа с EXl
     except:
         logging.exception(EXL)
         os.system("taskkill /f /im EXCEL.exe")
-        time.sleep(10)
+        time.sleep(5)
         EXL()
             
 def Out():  # Отправка в Outlook
     try:
-        fileors = glob.glob(os.path.join('C:/Users/*/Downloads/', 'ORS.xlsx'))
+        fileors = glob.glob(os.path.join('C:/Users/*/Downloads/', 'ORS*.xlsx'))
         for ors in fileors:
             pass
-        filejpg = glob.glob(os.path.join('C:/Users/*/Downloads/', 'ORS.jpg'))
+        filejpg = glob.glob(os.path.join('C:/Users/*/Downloads/', 'ORS*.jpg'))
         for jpg in filejpg:
             pass
         outlook = win32.Dispatch('outlook.application')
         mail = outlook.CreateItem(0)
-        mail.To = '' # Отправка почты
+        mail.To = 'ISmetskoy@alfabank.ru' # Отправка почты
         mail.Subject = 'Расчет ORS' 
         mail.Body = 'Расчет ORS на Дату: {day}'
         mail.HTMLBody =  "<html><body><h2>Расчет ORS на Дату: {day} <br></h2><img src=""cid:MyId1""></body></html>".format(day=day)
@@ -90,7 +118,7 @@ def Out():  # Отправка в Outlook
         logging.exception(Out)
         outlook = win32.Dispatch('outlook.application')
         mail = outlook.CreateItem(0)
-        mail.To = '' # Отправка почты
+        mail.To = 'ISmetskoy@alfabank.ru' # Отправка почты
         mail.Subject = 'Неудача подчета ORS'
         mail.Body = 'Неудача подчета ORS за {day}'
         mail.HTMLBody = "<html><body><h2>Неудача подчета ORS за {day}<br></h2></body></html>".format(day=day)
@@ -98,11 +126,12 @@ def Out():  # Отправка в Outlook
 
 def Delete():  # Удаление лишнего
     try:
-        time.sleep(30)
+        time.sleep(10)
+        os.system("taskkill /f /im EXCEL.exe")
         filedel = glob.glob(os.path.join
-            ('C:/Users/*/Downloads/', 'ORS.xlsx')) + glob.glob(os.path.join
-            ('C:/Users/*/Downloads/', 'ORS.jpg')) + glob.glob(os.path.join
-            ('C:/Users/*/Downloads/', 'detail_.xlsx')) 
+            ('C:/Users/*/Downloads/', 'ORS*.xlsx')) + glob.glob(os.path.join
+            ('C:/Users/*/Downloads/', 'ORS*.jpg')) + glob.glob(os.path.join
+            ('C:/Users/*/Downloads/', 'detail_*.xlsx')) 
         for delete in filedel:
             os.remove(delete)
             pass
@@ -110,4 +139,4 @@ def Delete():  # Удаление лишнего
     except:
         logging.exception(Delete)
 
-start = (Delete(), ORS(), Thread(target=TimeEXL).start(), EXL(), Out(), Delete()) # Поехали ;)
+start = (Delete(), ORS(), Thread(target=TimeEXL).start(), Thread(target=TimeKill).start(), EXL(), Out(), Delete()) # Поехали ;)
