fc7c6bb9bbb2e8951dab6818d0dcb0c029d62985
Ivan <felhat@bk.ru>
2023-10-02 11:02:15 +0300

    up

--- day.py
+++ day.py
@@ -9,7 +9,7 @@ log_dir = 'C:\\ORS\\log' # Проверка Пути для логов
 
 if not os.path.exists(log_dir):
     os.makedirs(log_dir, exist_ok=True)
-// напиши сайт
+
 log_file = os.path.join(log_dir, "day.log") # Запись лога в файл
 
 logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s %(levelname)s %(funcName)s || %(message)s')  # Конфигурация логов
